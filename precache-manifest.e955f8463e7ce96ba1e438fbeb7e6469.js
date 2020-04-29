c45711b7b5e29489f818997bc145e03397d3a476
Boris Adimov <boris@adimov.ru>
2020-04-29 19:09:00 +0300

    Updates

---
+++ precache-manifest.e955f8463e7ce96ba1e438fbeb7e6469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f6d21c74059266e7fb5319bca4bcf7",
+    "url": "/musicbrainz-test/index.html"
+  },
+  {
+    "revision": "6d45a118776e89e8a9ac",
+    "url": "/musicbrainz-test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7ffe8d59fd58130babb6",
+    "url": "/musicbrainz-test/static/js/2.fd081273.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/musicbrainz-test/static/js/2.fd081273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d45a118776e89e8a9ac",
+    "url": "/musicbrainz-test/static/js/main.20bd3bcd.chunk.js"
+  },
+  {
+    "revision": "9165989d8584d2b72fdf",
+    "url": "/musicbrainz-test/static/js/runtime-main.9e202094.js"
+  }
+]);
